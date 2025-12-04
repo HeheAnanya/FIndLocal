479208ee3b5c1656d64c2deae3a370ba4113c5cc
HeheAnanya <ananyanarang1411@gmail.com>
2025-12-04 11:05:07 +0530

    deploy

--- Backend/server.js
+++ Backend/server.js
@@ -10,8 +10,9 @@ const app = express()
 app.use(express.json())
 app.use(cors({
   origin: [
-    "https://f-ind-local.vercel.app"
-    // "http://localhost:5174"
+    "https://f-ind-local.vercel.app",
+   " https://findlocal.vercel.app",
+    "http://localhost:5174"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
